20131b0725c3789a5c18655c7acb11b0dbb04bb8
Anurag Gupta <anurag.gupta81@gmail.com>
2016-11-26 01:49:39 +0530

    Code
    Code with data

---
+++ run_analysis.R
@@ -0,0 +1,40 @@
+#Setting up the working directory
+setwd("C:/Users/Anurag_gupta11/Desktop/Secondary work stack/Data science/Quiz/Course 3 - Getting and cleaning data")
+
+
+#Reading all the txt files in Train folder
+xTrainDF <- read.table("train/X_train.txt")
+yTrainDF <- read.table("train/Y_train.txt")
+subjectTrainDF <- read.table("train/subject_train.txt")
+
+#reading all the txt files in test folder
+xTestDF <- read.table("test/X_test.txt")
+yTestDF <- read.table("test/Y_test.txt")
+subjectTestDF <- read.table("test/subject_test.txt")
+
+
+#Reading Activity file, features and features Info txt file
+activityLabels <- read.table("activity_labels.txt")
+features <- read.table("features.txt")
+
+colNames <- data.frame(features[,2])
+library(reshape)
+colNames <-t(colNames)
+
+#Merging X dataset together and Y dataset together Point 1
+data <- rbind(xTrainDF,xTestDF)
+yVar <- rbind(yTrainDF,yTestDF)
+
+#Getting labels mapped to the outcome
+labels <- merge(yVar,activityLabels,by=1)[,2]
+
+
+#Dataset labelled against Descriptive activity Point 4
+mergeyVar <- cbind(data,labels)
+
+
+TrainSet <- cbind(data,yVar)
+
+#Searching the Mean and Standard deviation column from the complete dataset Point 2
+search <- grep("-mean|-std", colnames(mergeyVar))
+data.mean.std <- mergeyVar[,c(1,2,search)]
